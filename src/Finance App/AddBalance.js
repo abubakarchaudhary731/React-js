14ca99a46dc82712a615a960e77a544aa4b9a5df
Abu Bakar <abubakarchaudhary731@gmail.com>
2023-09-05 20:03:07 +0500

    day 5

--- src/Finance App/AddBalance.js
+++ src/Finance App/AddBalance.js
@@ -1,62 +1,82 @@
-import React , {useState} from 'react';
-import TextField from '@mui/material/TextField';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import { GlobalContext } from '../Context';
+import { useNavigate } from 'react-router-dom';
 
-const AddBalance = () => {
-  const [form , setForm] = useState({
-    name: '',
-    amount: '',
-  })
+function FormExample() {
+  const {addData ,  handleChange, form, check, setCheck } = GlobalContext();
+  const navigate = useNavigate();
 
-  const [data , setData] = useState([]);
+  const [validated, setValidated] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm((prevData) => ({
-    ...prevData,
-      [name]: value
-    }))
-    
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      addData();
+      navigate('/')
+    }
 
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-  }
-
-// ADD Data
-  const addData = () =>{
-    setData([...data , {
-      name:form.name,
-      amount:form.amount,
-    }])
-    setForm({name: "" , amount: "" , })
-  }
+    setValidated(true);
+  };
 
   return (
-    <div>
-      <div className="container w-75">
+    <div className="container w-75">
       <div className="text-white text-center rounded-3 bg-primary">
         <h1 className='p-5'> Please Fill this Form. </h1>
       </div> <br /><br />
-
-        <form onSubmit={handleSubmit}>
-          <TextField 
-          value={form.name}
-          onChange={handleChange}
-          id="outlined-basic" label="Outlined" name='name' variant="outlined" />
-
-          <TextField 
-          value={form.amount}
-          onChange={handleChange}
-          id="outlined-basic" label="Outlined" name="amount" variant="outlined" />
-
-        <div className="text-center">
-          <button className='btn btn-primary' onClick={addData}> ADD </button>
-        </div>
-        </form>
-      </div>
+    <div className="text-center">
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3 justify-content-center">
+         
+        <Form.Group as={Col}  md="4" controlId="validationCustomUsername">
+          <Form.Label>Enter Your Name </Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+            value={form.name}
+            onChange={handleChange} name='name'
+              type="text" placeholder="Enter Name" aria-describedby="inputGroupPrepend" required />
+            <Form.Control.Feedback type="invalid">
+              Please Enetr your Name.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3 justify-content-center">
+      <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>Enter Your Amount <small> between (1-100k) </small> </Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+            value={form.amount}
+            onChange={handleChange} name='amount'
+              type="number" placeholder="Enter Amount" min={1} max={100000} maxLength={5}
+              aria-describedby="inputGroupPrepend" required />
+            <Form.Control.Feedback type="invalid">
+              Please Enetr your Amount.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check 
+         value={check}
+         onChange={(e) => setCheck(e.target.checked) } name='check' required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+      <Button type="submit"> ADD </Button>
+    </Form>
     </div>
-  )
+    </div>
+  );
 }
 
-export default AddBalance
+export default FormExample;
